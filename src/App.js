b7a5d228447c8f21e2b6449dd55edae94110b279
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2021-11-10 14:41:37 +0700

    connected to program

--- src/App.js
+++ src/App.js
@@ -1,65 +1,91 @@
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
-import idl from './idl.json';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Program, Provider, web3 } from "@project-serum/anchor";
+
+import idl from "./idl.json";
+
+const { SystemProgram, Keypair } = web3;
+
+let baseAccount = Keypair.generate();
+
+let programId = new PublicKey(idl.metadata.address);
+
+const network = clusterApiUrl("devnet");
+
+const opts = {
+  preflightCommitment: "processed"
+};
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const TEST_GIFS = [
-  'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
-  'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
-  'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
-  'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp',
+  "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
+  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
+  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
+  "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp"
 ];
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [gifList, setGifList] = useState([]);
 
   const checkWallet = async () => {
     try {
-      const {solana} = window;
+      const { solana } = window;
       if (solana && solana.isPhantom) {
-        console.log('Legit phantom wallet', solana);
-        const response = await solana.connect({onlyIfTrusted: true});
+        console.log("Legit phantom wallet", solana);
+        const response = await solana.connect({ onlyIfTrusted: true });
         // const response = await solana.request({
         //   method: 'connect',
         //   // params: {onlyIfTrusted: true},
         // });
         let newAddress = response.publicKey.toString();
-        console.log('Connected with public key :', newAddress);
+        console.log("Connected with public key :", newAddress);
         setWalletAddress(newAddress);
         return;
       }
 
-      alert('Solana object not found! Get a Phantom Wallet 👻');
+      alert("Solana object not found! Get a Phantom Wallet 👻");
     } catch (err) {
       console.error(err);
     }
   };
 
   const connectWallet = async () => {
-    const {solana} = window;
+    const { solana } = window;
     if (solana) {
       const response = await solana.connect();
       let newAddress = response.publicKey.toString();
-      console.log('Connected with public key', newAddress);
+      console.log("Connected with public key", newAddress);
       setWalletAddress(newAddress);
     }
   };
 
   const onInputChange = (e) => {
-    let {value} = e.target;
+    let { value } = e.target;
     setInputValue(value);
+  };
+
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+
+    const provider = new Provider(
+      connection,
+      window.solana,
+      opts.preflightCommitment
+    );
+    return provider;
   };
 
   const sendGif = async () => {
     if (inputValue.length > 0) {
-      console.log('Gif link:', inputValue);
+      console.log("Gif link:", inputValue);
     } else {
-      console.log('Empty input. Try again.');
+      console.log("Empty input. Try again.");
     }
   };
 
@@ -73,6 +99,19 @@ const App = () => {
   );
 
   const renderConnectedContainer = () => {
+    if (gifList === null) {
+      return (
+        <div className="connected-container">
+          <button
+            className="cta-button submit-gif-button"
+            onClick={createGifAccount}
+          >
+            Do One-Time create account
+          </button>
+        </div>
+      );
+    }
+
     return (
       <div className="connected-container">
         <input
@@ -96,21 +135,60 @@ const App = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('load', async () => {
+    window.addEventListener("load", async () => {
       await checkWallet();
     });
   }, []);
 
+  const getGifList = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+
+      console.log("got the account", account);
+      setGifList(account.gifList);
+    } catch (error) {
+      console.log("ERror getting gifs", error);
+      setGifList(null);
+    }
+  };
+
+  const createGifAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+      console.log("ping");
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId
+        },
+        signers: [baseAccount]
+      });
+      console.log(
+        "created a new baseAccount with address, ",
+        baseAccount.publicKey
+      );
+      await getGifList();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     if (walletAddress) {
-      console.log('Fetching gif list...');
-      setGifList(TEST_GIFS);
+      console.log("Fetching gif list...");
+      getGifList();
     }
   }, [walletAddress]);
 
   return (
     <div className="App">
-      <div className={walletAddress ? 'authed-container' : 'container'}>
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
           <p className="header">😂GIF IS LYFE🤯</p>
           <p className="sub-text">ETH MAXIS ✨✨✨✨ OFF </p>
